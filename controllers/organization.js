34022e439d6964c3e3fa3923f5b442036acc5d0c
Spencer Zhang <sepmein@gmail.com>
2015-01-06 18:56:21 +0800

    added some logic

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,17 +1,55 @@
 /**
  * Created by Spencer on 14/12/29.
  */
-"use strict";
+'use strict';
+
+var Organization = require('mongoose').model('Organization');
 
 exports.get = function *() {
-
+    let id = this.params.id;
+    try{ let found = yield Organization.findById(id).exec();
+        if(found){
+            this.body = found;
+        } else {
+            this.status = 404;
+            this.body = {
+                message: 'Organization does not exist.'
+            };
+        }
+    }
+    catch (error){
+        this.status = 400;
+        this.body = {
+            message: error.message
+        };
+    }
 };
+
 exports.add = function *() {
-
+    let org = new Organization(this.request.body.organization);
+    try {
+        let saved = yield org.save();
+        if(saved){
+            this.body = {
+                _id: saved._id
+            };
+        } else {
+            this.status = 400;
+            this.body = {
+                message: 'Some Error occurred. Save failed.'
+            };
+        }
+    } catch (error){
+        this.status = 400;
+        this.body = {
+            message: error.message
+        };
+    }
 };
-exports.list = function *() {
 
+exports.list = function *() {
+    Organization.find({});
 };
 exports.listStaff = function *() {
-
+    Organization.find({});
 };
